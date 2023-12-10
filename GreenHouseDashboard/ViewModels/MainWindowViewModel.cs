86ff1af2cc54d21d7575f3cdb26260c8cbe9b702
Manuel <manuelenzo2000@gmail.com>
2023-12-10 23:08:34 +0100

    Aggiungere i file di progetto.

---
+++ GreenHouseDashboard/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Avalonia;
+using Avalonia.Threading;
+using ReactiveUI;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace GreenHouseDashboard.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public MainWindowViewModel()
+        {
+            NowTime = DateTime.Now.ToString();
+            Task.Run(() => GetCurrent());
+        }
+
+
+        #region CurrentTime
+        public void GetCurrent()
+        {
+            while (true)
+            {
+                Dispatcher.UIThread.Invoke(new Action(() =>
+                {
+                    TimeNowCurrent = DateTime.Now;
+                    NowTime = TimeNowCurrent.ToString();
+                }));
+            }
+        }
+        #endregion
+
+
+        public string Greeting => "Benvenuto nella GreenHouseDashboard";
+
+
+        #region Property
+
+        private DateTime timeNowCurrent;
+
+        public DateTime TimeNowCurrent
+        {
+            get { return timeNowCurrent; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref timeNowCurrent, value);
+            }
+        }
+
+        private string nowTime;
+
+        public string NowTime
+        {
+            get { return nowTime; }
+            set { this.RaiseAndSetIfChanged(ref nowTime, value); }
+        }
+
+
+        #endregion
+    }
+}
---
+++ GreenHouseDashboard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace GreenHouseDashboard.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
